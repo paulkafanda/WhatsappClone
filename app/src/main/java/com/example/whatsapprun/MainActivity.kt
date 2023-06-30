f9334543cbab1d1eda2251980a9a99033139db9c
Paul kafanda <paulkaf84@gmail.com>
2023-06-30 21:48:39 +0200

    custom profile pic username message

--- app/src/main/java/com/example/whatsapprun/MainActivity.kt
+++ app/src/main/java/com/example/whatsapprun/MainActivity.kt
@@ -3,15 +3,17 @@ package com.example.whatsapprun
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -22,13 +24,15 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -37,6 +41,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.view.WindowCompat
 import com.example.whatsapprun.ui.theme.WhatsappRunTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import io.github.serpro69.kfaker.faker
 
 
 class MainActivity : ComponentActivity() {
@@ -45,18 +51,12 @@ class MainActivity : ComponentActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             WhatsappRunTheme(
-                darkTheme = false,
+                darkTheme = isSystemInDarkTheme(),
                 dynamicColor = true
             ) {
+                window.statusBarColor = colorScheme.primary.toArgb()
+                WhatsAppLayout()
 
-//                window.statusBarColor = colorScheme.primary.toArgb()
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    WhatsAppLayout()
-                }
             }
         }
     }
@@ -66,10 +66,36 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun WhatsAppLayout(){
 
+
+    val systemUiController = rememberSystemUiController()
+
+//                systemUiController.setSystemBarsColor(Color(0xFF118385), darkIcons = isSystemInDarkTheme())
+    systemUiController.setStatusBarColor(Color(0xFF118385), darkIcons = isSystemInDarkTheme())
+    systemUiController.setNavigationBarColor(Color.Transparent)
+
+
+
     val camera = painterResource(id = R.drawable.photo_camera_48px)
     val search = painterResource(id = R.drawable.search_48px)
     val more = painterResource(id = R.drawable.more_vert_48px)
     val group = painterResource(id = R.drawable.groups_48px)
+    val profilePics = setOf(
+        R.drawable.pro_0b36fe3068c6474aa8bddd33a48c272b_jpg_1200w_1200h_1s,
+        R.drawable.pro_095d2b032a754346b4bd84fa3ded5bff_jpg_1200w_1200h_1s,
+        R.drawable.pro_170388045e464228934ca7ed16413614_jpg_1200w_1200h_1s,
+        R.drawable.pro_0bddc5b6724347d7992a925c378d0a12_jpg_1200w_1200h_1s,
+        R.drawable.pro_11ca1b58ae6b49fb84f39d159d7c9c31_jpg_1200w_1200h_1s,
+        R.drawable.pro_128396fcfffa430c9f78c9a80265e20c_jpg_1200w_1200h_1s,
+        R.drawable.pro_1c237af0c9f3438787920d396d7319b9_jpg_1200w_1200h_1s,
+        R.drawable.pro_1e10f872512e4eafa273183122bdaf9f_jpg_1200w_1200h_1s,
+        R.drawable.pro_0bddc5b6724347d7992a925c378d0a12_jpg_1200w_1200h_1s,
+        R.drawable.pro_29a928b0f6e64a7e9ddcbf743097b92a_jpg_1200w_1200h_1s,
+        R.drawable.pro_338f6c8fec624b9c89095adf29c4ee13_jpg_1200w_1200h_1s,
+        R.drawable.pro_49c28c1458f241fbb3e97105c92288aa_jpg_1200w_1200h_1s,
+        R.drawable.pro_372d76e5582443f4a0a8973a83c03569_jpg_1200w_1200h_1s,
+        R.drawable.pro_856f8dc0f4be4d3498bcc3aa519f201d_jpg_1200w_1200h_1s,
+    )
+    val faker = faker {  }
 
     Column {
         Column(
@@ -138,7 +164,7 @@ fun WhatsAppLayout(){
                 )
                 Text(
                     text = "Chats",
-                    color = Color.White, fontFamily = FontFamily.SansSerif
+                    color = Color.White, fontFamily = FontFamily.SansSerif,
                 )
                 Text(
                     text = "Status",
@@ -163,7 +189,7 @@ fun WhatsAppLayout(){
         ) {
             repeat(20) {
                 Spacer(modifier = Modifier.height(12.dp))
-                Card()
+                Card(profilePics.random(), faker.name.neutralFirstName(), faker.bigBangTheory.quotes().substring(0..7))
                 Spacer(modifier = Modifier.height(12.dp))
             }
         }
@@ -173,7 +199,11 @@ fun WhatsAppLayout(){
 }
 
 @Composable
-fun Card() {
+fun Card(
+    @DrawableRes image: Int = R.drawable.pro_170388045e464228934ca7ed16413614_jpg_1200w_1200h_1s,
+    userName: String,
+    message: String
+) {
     Row(
         modifier = Modifier
             .fillMaxWidth(),
@@ -181,11 +211,13 @@ fun Card() {
         horizontalArrangement = Arrangement.SpaceAround
     ) {
         Image(
-            painter = painterResource(id = R.drawable.unj05av8_400x400),
+            painter = painterResource(id = image),
             contentDescription = "user",
             modifier = Modifier
                 .size(52.dp)
                 .clip(CircleShape)
+                .clickable {
+                }
         )
 
         Spacer(modifier = Modifier.width(12.dp))
@@ -200,7 +232,7 @@ fun Card() {
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
                 Text(
-                    text = "John Doe",
+                    text = userName,
                     color = Color(R.color.gray_10),
                     fontSize = 18.sp,
                     fontWeight = FontWeight.SemiBold
@@ -216,9 +248,9 @@ fun Card() {
                     .fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Text(text = "Hey, what's up ?", color = Color(R.color.gray_500))
+                Text(text = message, color = Color(R.color.gray_500))
                 Text(
-                    text = "28",
+                    text = (1..19).random().toString(),
                     color = Color.White,
                     fontSize = 12.sp,
                     modifier = Modifier
@@ -236,6 +268,243 @@ fun ImageViewer() {
     Box(modifier = Modifier) {
 
     }
+}
+
+@Composable
+fun MyStatusComponent() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        Box(contentAlignment = Alignment.BottomEnd) {
+            Image(
+                painter = painterResource(id = R.drawable.pro_095d2b032a754346b4bd84fa3ded5bff_jpg_1200w_1200h_1s),
+                contentDescription = "user",
+                modifier = Modifier
+                    .size(52.dp)
+                    .clip(CircleShape)
+                    .clickable {
+                    }
+            )
+            Box(
+                modifier = Modifier
+                    .background(Color.Transparent),
+                contentAlignment = Alignment.Center
+            ) {
+                Box(modifier = Modifier
+                    .size(20.dp)
+                    .background(Color.White, ShapeDefaults.ExtraLarge))
+                Icon(
+                    painter = painterResource(R.drawable.add_48px),
+                    contentDescription = "Add",
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .size(16.dp)
+                        .background(Color(0xFF118385)),
+                    tint = Color.White
+
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.width(12.dp))
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = "My status",
+                    color = Color(R.color.gray_10),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = "Tap to add status update",
+                    color = Color(R.color.gray_500),
+                    fontSize = 14.sp
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun HeaderComponent() {
+    val camera = painterResource(id = R.drawable.photo_camera_48px)
+    val search = painterResource(id = R.drawable.search_48px)
+    val more = painterResource(id = R.drawable.more_vert_48px)
+    val group = painterResource(id = R.drawable.groups_48px)
+
+    Column(
+        modifier = Modifier
+            .background(Color(0xFF118385))
+    ) {
+        Row(
+            modifier = Modifier,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "WhatsApp",
+                fontFamily = FontFamily.SansSerif,
+                fontSize = 28.sp,
+                modifier = Modifier
+                    .fillMaxWidth(0.7f)
+                    .padding(start = 12.dp),
+                color = Color.White
+            )
+
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    painter = camera,
+                    contentDescription = "more",
+                    tint = Color(0xFFFFFFFF),
+                    modifier = Modifier
+                        .size(28.dp)
+                )
+                Icon(
+                    painter = search,
+                    contentDescription = "camera",
+                    tint = Color(0xFFFFFFFF),
+                    modifier = Modifier
+                        .size(28.dp)
+                )
+                Icon(
+                    painter = more,
+                    contentDescription = "search",
+                    tint = Color(0xFFFFFFFF),
+                    modifier = Modifier
+                        .size(28.dp)
+                )
+            }
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            Icon(
+                painter = group,
+                contentDescription = "group",
+                tint = Color.White,
+                modifier = Modifier
+                    .size(32.dp)
+            )
+            Text(
+                text = "Chats",
+                color = Color.White, fontFamily = FontFamily.SansSerif,
+            )
+            Text(
+                text = "Status",
+                color = Color.White,
+                fontFamily = FontFamily.SansSerif
+            )
+            Text(
+                text = "Calls",
+                color = Color.White,
+                fontFamily = FontFamily.SansSerif
+            )
+
+        }
+    }
+}
+
+
+@Composable
+fun StatusComponent() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        Box(contentAlignment = Alignment.BottomEnd) {
+            Image(
+                painter = painterResource(id = R.drawable.pro_0b36fe3068c6474aa8bddd33a48c272b_jpg_1200w_1200h_1s),
+                contentDescription = "user",
+                modifier = Modifier
+                    .size(52.dp)
+                    .clip(CircleShape)
+                    .clickable {
+                    }
+            )
+        }
+
+        Spacer(modifier = Modifier.width(12.dp))
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = "Miley",
+                    color = Color(R.color.gray_10),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = "23 minutes ago",
+                    color = Color(R.color.gray_500),
+                    fontSize = 14.sp
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HeaderComponentPreview() {
+    MaterialTheme {
+        HeaderComponent()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MyStatusComponentPreview() {
+    MaterialTheme {
+        MyStatusComponent()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun StatusComponentPreview() {
+    MaterialTheme{
+        StatusComponent()
+    }
+
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/example/whatsapprun/mainscript.kt
+++ app/src/main/java/com/example/whatsapprun/mainscript.kt
@@ -1,5 +1,33 @@
 package com.example.whatsapprun
 
-fun main() {
-    println("try Hello")
+import androidx.annotation.IdRes
+
+class ImageResource(
+    var previous: ImageResource?,
+    var next: ImageResource?,
+    @IdRes val resId:  Int
+)
+
+class DoubledLinkedList(
+    private var cursor: ImageResource?,
+    private var start: ImageResource?,
+    private var end: ImageResource
+) {
+    private var size: Int = 0
+
+    private val set: MutableSet<ImageResource> = mutableSetOf()
+    fun add(imageResource: ImageResource) {
+        if (set.size == 0) {
+            cursor = imageResource
+            start = imageResource
+        }
+
+        end = imageResource
+        set.add(imageResource)
+        size++
+    }
+
+    fun next() {
+
+    }
 }
